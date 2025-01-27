531937d6130d5de6bb9f984dcd2d71038e9afeb2
sulamithh <sulamif.kz@yandex.kz>
2025-01-27 23:07:40 +0800

    Add index page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to Gafur's Website!</h1>
+        <p>This is a placeholder homepage. Start building your content here.</p>
+      </div>
+    );
+  }
+  

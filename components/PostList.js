a99902f343b4b6acc079300dc08f66f576d64705
sulamithh <sulamif.kz@yandex.kz>
2025-01-31 00:06:46 +0800

    Uploaded design

--- components/PostList.js
+++ components/PostList.js
@@ -4,7 +4,7 @@ export default function PostList({ posts }) {
   return (
     <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-6">
       {posts.map((post) => (
-        <Link key={post.slug} href={`/post/${post.slug}`}>
+        <Link key={post.slug} href={`/${post.slug}`}>
           <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition duration-300 cursor-pointer border border-gray-200">
             <h2 className="text-xl font-semibold text-gray-900">{post.title}</h2>
             <p className="text-gray-600 mt-2">{post.date}</p>
